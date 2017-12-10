f94df403bc583516eb4d4a696264bdaa94af30e9
shusaku.miyahara <miyahra@gmail.com>
2017-12-10 17:36:32 +0900

    initial commit

---
+++ src/main/java/com/example/fleamarketapi/FleaMarketApiApplication.java
@@ -0,0 +1,12 @@
+package com.example.fleamarketapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleaMarketApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleaMarketApiApplication.class, args);
+	}
+}
